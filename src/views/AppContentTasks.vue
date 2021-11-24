29400528c6069248ee6c663c148fdcc26b061efa
GaliaBonda <galina.rizhniak@gmail.com>
2021-11-24 23:48:15 +0200

    Lesson 7: Form to create new task

--- src/views/AppContentTasks.vue
+++ src/views/AppContentTasks.vue
@@ -2,6 +2,20 @@
 .content.main__content
   .content__container
     h2.content__date Today tasks
+    form.record-form
+      h2.record-form__title Add new task
+      .record-form__container
+        .record-form__item
+          label.record-form__name-label(for="name") New task name:
+          input.record-form-element.record-form__name(type="text" id="name")
+        .record-form__item
+          label.record-form__desc-label(for="desc") New task description:
+          textarea.record-form-element.record-form__desc(id="desc" rows="4" cols="50")
+        .record-form__item
+          label.record-form__date-label(for="date") New task deadline:
+          input.record-form-element.record-form__date(type="date" id="date")
+        .record-form__item
+          input.record-form-element.record-form__btn(type="submit" id="btn" value="Add task" v-on:click="addTask")
     .content__records
       .record.content__record(v-for="item in tasks" v-bind:key="item.id")
         h3.record__title {{item.name}}
@@ -18,20 +32,20 @@ const tasks: Task[] = [
   {
     name: 'Design mockup',
     desc: "Create full rendering of design for client's web-product. Client K&H Science",
-    deadLine: '12.11',
+    deadLine: '12.11.2021',
     id: 111,
   },
   {
     name: 'Analysis of requirements and outcomes',
     desc: 'Evaluate the product design and development against project requirements and outcomes',
-    deadLine: '14.12',
-    id: 222,
+    deadLine: '14.12.2021',
+    id: 112,
   },
   {
     name: 'Application Testing',
     desc: 'Identify errors in a website, provide unit, system and functional testing',
-    deadLine: '21.11',
-    id: 333,
+    deadLine: '21.11.2021',
+    id: 113,
   },
 ];
 
@@ -42,15 +56,76 @@ export default defineComponent({
       tasks: tasks,
     };
   },
+  methods: {
+    addTask(e: Event) {
+      e.preventDefault();
+      const recordName = document.getElementById('name') as HTMLInputElement;
+      const recordDesc = document.getElementById('desc') as HTMLTextAreaElement;
+      const recordDate = document.getElementById('date') as HTMLInputElement;
+      const date = new Date(recordDate.value).toLocaleDateString();
+      if (recordName.value && recordDesc.value && recordDate.value) {
+        this.tasks.push({
+          name: recordName.value,
+          desc: recordDesc.value,
+          deadLine: date,
+          id: tasks[tasks.length - 1].id + 1,
+        });
+        recordName.value = '';
+        recordDesc.value = '';
+        recordDate.value = '';
+      }
+    },
+  },
 });
 </script>
 
 <style scoped lang="scss" src="../styles/scss/content.scss"></style>
 <style scoped lang="scss">
+@import '../styles/scss/styles.scss';
+
 .record__title {
   padding: 10px;
   opacity: 0.5;
   font-size: 12px;
   text-transform: uppercase;
+}
+.record-form {
+  padding: 20px 30px 30px 30px;
+  border-top: 5px solid $bg-color;
+  border-bottom: 5px solid $bg-color;
+}
+.record-form__title {
+  padding: 10px 0;
+  opacity: 0.5;
+  font-size: 12px;
+  text-transform: uppercase;
+}
+.record-form__container {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding-top: 20px;
+}
+.record-form__item {
+  display: flex;
+  justify-content: space-between;
+}
+.record-form-element {
+  padding: 5px;
+  font-family: Helvetica, sans-serif;
+  border-radius: 3px;
+  border: 2px solid $share-bg;
+}
+.record-form__btn {
+  background-color: $bg-color;
+  border-radius: 3px;
+  border: 2px solid $share-bg;
+  padding: 5px;
+}
+
+.record-form__btn:hover {
+  border: 2px solid $bg-color;
+  color: $more-font;
+  cursor: pointer;
 }
 </style>
